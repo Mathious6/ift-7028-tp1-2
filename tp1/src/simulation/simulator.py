91525c425173573055ec15e99d33bd232e8e4385
Pascal de_Le_Rue <pascal.de-le-rue.1@ulaval.ca>
2025-05-15 21:04:44 +0000

    corrected data printed

--- tp1/src/simulation/simulator.py
+++ tp1/src/simulation/simulator.py
@@ -1,3 +1,5 @@
+import os
+import time
 from src.models.airplane import Airplane
 from .events import Event
 from queue import Queue
@@ -22,13 +24,14 @@ class Simulator:
                 airplane = Airplane()
                 airplaines_waiting_queue.put(airplane)
                 number_of_airplane_arrivals += 1
-                print(f"Airplane {airplane.id} added to airport waiting queue.")
+                print(f"Airplane {airplane.id} added to airport waiting queue at {index_of_time} time.")
             
             if remaining_robots_work_time > 0:
                 remaining_robots_work_time -= 1
                 if remaining_robots_work_time <= 0:
                     number_of_airplane_discharged += 1
                     remaining_robots_work_time = 0
+                    print(f"Airplane discharged at {index_of_time} time.")
 
             else:
                 if not airplaines_waiting_queue.empty():
@@ -36,7 +39,7 @@ class Simulator:
 
                     airplane: Airplane = airplaines_waiting_queue.get()
                     
-                    print(f"Airplane {airplane.id} discharging.")
+                    print(f"Airplane {airplane.id} discharging at {index_of_time} time.")
                     
                 else:
                     time_waited_by_robots += 1
@@ -45,6 +48,7 @@ class Simulator:
         print(f"Number of airplanes arrived: {number_of_airplane_arrivals}")
         print(f"Number of airplanes discharged: {number_of_airplane_discharged}")
         print(f"Time waited by robots: {time_waited_by_robots / 10}")
+        print(f"Work time remaining for robots: {remaining_robots_work_time / 10}")
         
 
 
